5856b3f55353a15ea94a3e1bb9997e41ed00eb45
*Lahari-410* <*laharibitla27@gmail.com*>
2026-03-26 23:25:55 +0530

    final fix ignore typescript errors

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -10,3 +10,6 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+// ✅ NEVER NULL
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey)
