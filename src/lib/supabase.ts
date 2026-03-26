c542552b060931ac138b0765bd0231938ca160b9
*Lahari-410* <*laharibitla27@gmail.com*>
2026-03-26 22:47:39 +0530

    fix supabase admin null error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -10,5 +10,3 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey)
